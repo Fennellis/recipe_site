9185edb2f85a8d71b63f197295472d419b545768
Fennellis <westgeze@gmail.com>
2024-10-01 14:21:35 +0300

    Add models

--- app/models.py
+++ app/models.py
@@ -1,12 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class Recipe(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=100)
     description = models.TextField()
-    steps = models.Field()
-    cooking_time = models.TimeField()
-    image = models.ImageField
-    author = models.ForeignKey()
+    cooking_time = models.DurationField()
+    image = models.ImageField(upload_to='recipes/')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class CookingSteps(models.Model):
+    recipe = models.ForeignKey('Recipe', on_delete=models.CASCADE, related_name='steps')
+    step_number = models.PositiveIntegerField()
+    description = models.TextField()
+    image = models.ImageField('steps/')
