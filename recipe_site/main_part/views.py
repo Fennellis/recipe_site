7cc7a90a1527c154752cad38316d09721ace666e
Fennellis <westgeze@gmail.com>
2024-11-01 20:13:58 +0300

    Add html-structure and home page

--- recipe_site/main_part/views.py
+++ recipe_site/main_part/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+
+class HomeView(View):
+    def get(self, request) -> HttpResponse:
+        return render(request, 'main_part/home.html', context={})
