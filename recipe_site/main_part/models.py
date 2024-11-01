76691a8945234e5b09d9910c1b165bfb517915d1
Fennellis <westgeze@gmail.com>
2024-11-01 19:41:39 +0300

    Add Recipe, RecipeSteps and RecipeCategory models

--- recipe_site/main_part/models.py
+++ recipe_site/main_part/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class RecipeCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    steps = models.TextField()
+    cooking_time = models.IntegerField()
+    picture = models.ImageField(upload_to='recipes/')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes')
+    category = models.ManyToManyField(RecipeCategory, related_name='recipes')
+
+
+class RecipeSteps(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    number = models.IntegerField()
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    picture = models.ImageField(upload_to='recipe_steps/')
